7c6de130885c5541ad0ab5c2c7a3cca27850d2b0
Bitwise <bitwise@bitwise-server.(none)>
2011-03-04 21:23:13 +0530

    added worker hosting script

---
+++ host_workers_zip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd workers
+find . -name "*.pyc" -exec rm '{}' ';'
+mkdir workers 
+cp -R base_worker workers/
+cp  README workers/
+cp  worker.zip workers/
+cp  exec_worker.sh workers/
+zip -r ../web_interface/media/workers.zip workers
+rm -r workers
+echo 'The zip is now hosted at: "http://DOMAIN/media/workers.zip"' 
